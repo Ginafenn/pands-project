15338adc01170cc336d8e06cfd4d3c693495ce92
Ginafenn <regina.fennessy@bsci.com>
2022-04-27 20:05:21 +0100

    analysis

--- analysis.py
+++ analysis.py
@@ -1,2 +1,19 @@
 # Fisher Iris Data Set
 # Author: Regina Fennessy
+
+f = open('Fisher Iris Data Set.csv', 'r')
+lines = f.readlines()
+
+data = []
+label = []
+
+for line in lines[ 1: ]:
+    line = line.replace('\n', '').split(',')
+    
+    # Convert string data into float form
+    data.append( list(map(float, line[ :-1 ])) )
+    label.append( line[-1] )
+    
+print(data)
+print()
+print(label)
