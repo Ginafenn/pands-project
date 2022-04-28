fcab27407da583cd75d26524efccd6e37a8a7e03
Ginafenn <regina.fennessy@bsci.com>
2022-04-28 14:37:36 +0100

    Updated Version

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 # Author: Regina Fennessy
 
 
-The Fisher Data set was first introduced by Ronald Fisher in 1936.
+The Fisher Data set was first introduced by Ronald Fisher in 1936. Ronald is a british statistician and biologist.It is one of the most used data set
 It is made up of 50 data sets from 3 different species of the Iris flower. 
 This was used to quantify the variations of the three species.
 
@@ -21,8 +21,22 @@ Petal width in cm
 He created a linear discriminant model which was used to tell the three species apart.
 
 
-Code:
+Libraries Used for this Project
 
-Import NumPy for scientific computing in python
-Import Pandas for data analysis in python
-Import Seaborn data visualisation library based on matplotlib
+NumPy: Numpy or Numerical Python was created in 2005 by Travis Oliphant. Is a fundamental package for scientific computing with Python. It is used for working in domain of linear alegbra fourier and matrices
+Pandas: Is an open source library used for data analysis/data science and machine learning tasks in python. It is built on top of Numpy which provides support for multi -dimensional arrays
+Seaborn: Is a data visualisation library based on matplotlib. It provides a high level interface for drawing attractive and informative statistical graphics
+matplotlib: Is a library for creating static and interactive visualisation in Python
+
+
+
+
+Reference:
+
+https://en.wikipedia.org/wiki/Iris_flower_data_set
+https://seaborn.pydata.org/
+https://www.w3schools.com/python/numpy/numpy_intro.asp
+https://pandas.pydata.org/
+
+https://www.geeksforgeeks.org/box-plot-and-histogram-exploration-on-iris-data/
+
--- analysis.py
+++ analysis.py
@@ -3,21 +3,55 @@
 
 import numpy as np
 import pandas as pd
-import seaborn as sb
+import seaborn as sns
+from matplotlib import pyplot as plt
 
-f = open('Fisher Iris Data Set.csv', 'r')
-lines = f.readlines()
 
-data = []
-label = []
+#Read the csv data set
+data = pd.read_csv('Fisher Iris Data Set.csv')
+data.head()
 
-for line in lines[ 1: ]:
-    line = line.replace('\n', '').split(',')
-    
-    # Convert string data into float form
-    data.append( list(map(float, line[ :-1 ])) )
-    label.append( line[-1] )
-    
-print(data)
-print()
-print(label)
+# This is to identify headings of datasets
+Name = ['sepal_length','sepal_width', 'petal_length', 'petal_width', 'species']
+
+#To prove how many columns and rows
+print(data.shape)
+data.describe()
+
+#Create a text file that shows a summary of the dataset
+with open("dataset.txt","a") as f:
+      print((data.describe()),file = f)
+
+#x = Name["sepal_length"]
+
+
+#Create histogram for Sepal Length using black edging/creating a title/and labels for x and y axis
+data.hist(column='sepal_length',edgecolor='black') 
+plt.title('Sepal Length') 
+plt.xlabel('Length (cm)') 
+plt.ylabel('Quantity') 
+plt.savefig('Sepal_Length.png')
+#plt.show()
+
+#Create histogram for Sepal Width using black edging/creating a title/and labels for x and y axis
+data.hist(column='sepal_width',edgecolor='black') 
+plt.title('Sepal Width') 
+plt.xlabel('Length (cm)') 
+plt.ylabel('Quantity') 
+plt.savefig('Sepal_Width.png')
+
+#Create histogram for Petal Length using black edging/creating a title/and labels for x and y axis
+data.hist(column='petal_length',edgecolor='black') 
+plt.title('Petal Length') 
+plt.xlabel('Length (cm)') 
+plt.ylabel('Quantity') 
+plt.savefig('Petal_Length.png')
+
+#Create histogram for Petal Width using black edging/creating a title/and labels for x and y axis
+data.hist(column='petal_width',edgecolor='black') 
+plt.title('Petal Width') 
+plt.xlabel('Length (cm)') 
+plt.ylabel('Quantity') 
+plt.savefig('Petal_Width.png')
+
+
